9801b51547c2d71cab832a7ed5451f40309963d6
Bagas Harmadi <71679110+bagasharmadi@users.noreply.github.com>
2021-11-03 15:14:57 +0700

    Update settings.py

--- MYWEBSITE basedir/MYWEBSITE/settings.py
+++ MYWEBSITE basedir/MYWEBSITE/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'uh@xz+$cxv%y4jp)h6_)clj4#g+*!7=*cl!*#$*_jbc4vfuls1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [https://git.heroku.com/bagasharmadii.git]
 
 
 # Application definition
